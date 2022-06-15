aa65175dfd5192e6c7477d0cc28d7863e6213f0b
Abelardo Aqui <abelardo.aqui@eon.com.mx>
2022-06-15 14:45:54 -0500

    sorted lua file

--- lua/init.lua
+++ lua/init.lua
@@ -1,58 +1,41 @@
-local lsp_defaults = {
-  flags = {
-    debounce_text_changes = 150,
-  },
-  capabilities = require('cmp_nvim_lsp').update_capabilities(
-    vim.lsp.protocol.make_client_capabilities()
-  ),
-  on_attach = function(client, bufnr)
-    vim.api.nvim_exec_autocmds('User', {pattern = 'LspAttached'})
+local lsp_installer = require("nvim-lsp-installer")
+local luasnip = require("luasnip")
+lsp_installer.on_server_ready(function(server)
+  local opts = {}
+  if server.name == "sumneko_lua" then
+    opts = {
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { 'vim', 'use' }
+          },
+        }
+      }
+    }
   end
-}
-local lspconfig = require('lspconfig')
-lspconfig.emmet_ls.setup({
-    -- on_attach = on_attach,
-    capabilities = capabilities,
-    filetypes = { 'html', 'typescriptreact', 'javascriptreact', 'css', 'sass', 'scss', 'less' },
-})
-lspconfig.util.default_config = vim.tbl_deep_extend(
-  'force',
-  lspconfig.util.default_config,
-  lsp_defaults
-)
+  server:setup(opts)
+end)
+ -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions 
+local bufopts = { noremap=true, silent=true } 
+vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts) 
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts) 
+vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts) 
+vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts) 
+vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts) 
+vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts) 
+vim.keymap.set('n', '<space>wl', function() 
+print(vim.inspect(vim.lsp.buf.list_workspace_folders())) 
+end, bufopts) 
+vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts) 
+vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts) 
+vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts) 
+vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts) 
+vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
 
-local opts = { noremap=true, silent=true }
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
 
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  local bufopts = { noremap=true, silent=true, buffer=bufnr }
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-  vim.keymap.set('n', '<space>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, bufopts)
-  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
-end
-
--- luasnip setup
-local luasnip = require 'luasnip'
-
--- nvim-cmp setup
+-- cmp vim
+--
 local cmp = require 'cmp'
 cmp.setup {
   snippet = {
@@ -97,31 +80,4 @@ cmp.setup {
     { name = 'nvim_lsp' },
   },
 }
--- Setup lspconfig.
-  local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-  -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-  -- require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
-  --   capabilities = capabilities
-  -- }
--- Use a loop to conveniently call 'setup' on multiple servers and
--- map buffer local keybindings when the language server attaches
--- Add additional capabilities supported by nvim-cmp
--- local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-require("nvim-lsp-installer").setup {}
-local servers = { 'tsserver', 'angularls', 'html', 'emmet_ls', 'cssls', 'cssmodules_ls', 'tailwindcss' }
-for _, lsp in pairs(servers) do
-  require('lspconfig')[lsp].setup {
-    capabilities = capabilities,
-    on_attach = on_attach,
-    flags = {
-      -- This will be the default in neovim 0.7+
-      debounce_text_changes = 150,
-    }
-  }
-end
-
-
-
 
