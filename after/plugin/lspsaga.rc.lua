bc2210dcd740da58cc1367c4a6d5e22fbe7565f5
TecAbel <abelardo96.work@gmail.com>
2023-02-03 09:19:21 -0600

    feat(lsp-saga): changed to newest

---
+++ after/plugin/lspsaga.rc.lua
@@ -0,0 +1,14 @@
+local saga = require('lspsaga')
+
+-- saga.setup({
+--   ui = {
+--     winblend = 10,
+--     border = 'rounded',
+--     colors = {
+--       normal_bg = '#002b36'
+--     }
+--   },
+--   symbol_in_winbar = {
+--     enable = false,
+--   }
+-- })
