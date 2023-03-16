231201f82a4444102ec6f2ebe38a400383b729f0
TecAbel <abelardo96.work@gmail.com>
2023-03-16 10:18:45 -0600

    fix(c-issue): <leader>ca to code action

--- after/plugin/telescope-fbrowser.rc.lua
+++ after/plugin/telescope-fbrowser.rc.lua
@@ -9,7 +9,7 @@ end
 -- IMPORTANT!: this is only a showcase of how you can set default options!
 telescope.setup {
   defaults = {
-    file_ignore_patterns = { "node_modules" },
+    file_ignore_patterns = { "node_modules", ".git" },
     mappings = {
       n = {
         ["q"] = actions.close
