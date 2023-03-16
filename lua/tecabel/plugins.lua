8542637de5b2f1d5afa5a25f024a6f80d2a85b0f
TecAbel <abelardo96.work@gmail.com>
2023-03-16 14:27:27 -0600

    feat(coc): release br

--- lua/tecabel/plugins.lua
+++ lua/tecabel/plugins.lua
@@ -1,13 +1,15 @@
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
+  vim.fn.system({
+
+    "git",
+    "clone",
+
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
     lazypath,
-  }
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -66,7 +68,7 @@ require('lazy').setup({
       -- show_trailing_blankline_indent = false,
     },
   },
-  'neoclide/coc.nvim',
+  { 'neoclide/coc.nvim', branch = "release" },
   'alvan/vim-closetag',
   {
     'windwp/nvim-autopairs',
