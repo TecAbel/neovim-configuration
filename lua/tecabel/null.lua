f201ffd478df4e7d2ea5d336bb718384975ba18a
TecAbel <abelardo96.work@gmail.com>
2023-05-14 23:43:05 -0600

    feat(cs): formatting with csharpier

--- lua/tecabel/null.lua
+++ lua/tecabel/null.lua
@@ -10,6 +10,7 @@ null_ls.setup({
     null_ls.builtins.formatting.black,
     null_ls.builtins.formatting.prettier,
     null_ls.builtins.diagnostics.eslint,
+    null_ls.builtins.formatting.csharpier,
   },
   on_attach = function(client, bufnr)
     if client.supports_method("textDocument/formatting") then
