1b6bd2d7091b9262dfd28cb10b5cb7fe9ec675b8
TecAbel <abelardo96.work@gmail.com>
2023-12-14 23:43:22 -0600

    feat(coc): removed tsserver

--- after/plugin/coc.rc.lua
+++ after/plugin/coc.rc.lua
@@ -1,6 +1,6 @@
 
 vim.cmd([[
 autocmd FileType dart,typescript,typescriptreact,javascript let b:coc_suggest_disable = 1
-let g:coc_global_extensions = ['coc-explorer', 'coc-lists', 'coc-tsserver', 'coc-db']
+let g:coc_global_extensions = ['coc-explorer', 'coc-lists', 'coc-db']
 ]])
 
