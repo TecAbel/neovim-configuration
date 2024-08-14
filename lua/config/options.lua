efc7ae2f005057d21507d9138da4316c048d80f8
TecAbel <abelardo96.work@gmail.com>
2024-08-13 20:35:32 -0600

    feat(incline): added

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,4 +5,5 @@
 -- Undercurl
 
 vim.cmd([[let &t_Cs = "\e[4:3m]"]])
-vim.cmd([[let &t_Ce = "\e[4:0m]"]])
+vim.cmd([[let &t_Ce = "\e[4:3m]"]])
+vim.opt.termguicolors = true
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -26,6 +26,7 @@ return {
         mode = "tabs",
         show_buffer_close_icons = false,
         show_close_icon = false,
+        diagnostics = "nvim_lsp",
       },
     },
   },
@@ -57,9 +58,50 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+      },
       window = {
         position = "right",
       },
     },
+  },
+  -- filename
+  {
+    "b0o/incline.nvim",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    event = "VeryLazy",
+    priority = 1200,
+    config = function()
+      local helpers = require("incline.helpers")
+      local devicons = require("nvim-web-devicons")
+      require("incline").setup({
+        window = {
+          padding = 0,
+          margin = { horizontal = 0 },
+        },
+        render = function(props)
+          local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+          if filename == "" then
+            filename = "[No Name]"
+          end
+          local ft_icon, ft_color = devicons.get_icon_color(filename)
+          local modified = vim.bo[props.buf].modified
+          return {
+            ft_icon and { " ", ft_icon, " ", guibg = ft_color, guifg = helpers.contrast_color(ft_color) } or "",
+            " ",
+            { filename, gui = modified and "bold,italic" or "bold" },
+            " ",
+            guibg = "#d79921",
+          }
+        end,
+      })
+    end,
   },
 }
