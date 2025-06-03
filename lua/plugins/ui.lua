3bea9ed4096449602547f9dbcfb78621d11e3a7b
TecAbel <abelardo96.work@gmail.com>
2025-06-03 16:33:56 -0600

    feat(incline): added new colors and modified section

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -72,29 +72,28 @@ return {
     dependencies = {
       "nvim-tree/nvim-web-devicons",
     },
-    event = "VeryLazy",
+    event = "BufReadPre",
     priority = 1200,
     config = function()
-      local helpers = require("incline.helpers")
-      local devicons = require("nvim-web-devicons")
       require("incline").setup({
         window = {
           padding = 0,
-          margin = { horizontal = 0 },
+          margin = { horizontal = 2, vertical = 0 },
         },
         render = function(props)
           local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
           if filename == "" then
             filename = "[No Name]"
           end
-          local ft_icon, ft_color = devicons.get_icon_color(filename)
-          local modified = vim.bo[props.buf].modified
+          if vim.bo[props.buf].modified then
+            filename = "[+] " .. filename
+          end
+          local icon = require("nvim-web-devicons").get_icon_color(filename)
           return {
-            ft_icon and { " ", ft_icon, " ", guibg = ft_color, guifg = helpers.contrast_color(ft_color) } or "",
-            " ",
-            { filename, gui = modified and "bold,italic" or "bold" },
-            " ",
-            guibg = "#d79921",
+            { " ", icon, guibg = "#bdae93", guifg = "#076678", " " },
+            { " " },
+            { filename, " ", guifg = "#1d2021" },
+            guibg = "#a89984",
           }
         end,
       })
